2a185bfb20933a25cf63ed6d9b0edfce5b5a993d
Baptiste Harle <baptiste.harle@gmail.com>
2021-11-04 20:37:34 +0100

    ajout Manage

--- frontend/src/pages/index.js
+++ frontend/src/pages/index.js
@@ -1,8 +1,9 @@
 import Login from './Login';
 import Signup from './Signup';
 import Wall from './Wall';
+import Manage from './Manage';
 
-const pages = { Login, Signup, Wall, };
+const pages = { Login, Signup, Wall, Manage};
 
-export { Login, Signup, Wall, };
+export { Login, Signup, Wall, Manage, };
 export default pages;
