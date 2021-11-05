89e7acf5367e4d38dfb11542eb123ad45703790e
Baptiste Harle <baptiste.harle@gmail.com>
2021-11-05 17:44:59 +0100

    rectification bug email

--- backend/src/controllers/userController.js
+++ backend/src/controllers/userController.js
@@ -4,7 +4,7 @@ import { createJWT } from '../utils/tools';
 
 class UserController {
   static async getUser(req, res, next) {
-    const user = await User.findOne({ where: { email: req.user.email }});
+    const user = await User.findOne({ where: { email: req.user.email } });
     res.json({ code: 200, msg: "Voici les informations de l'utilisateur.", data: { user }, errors: [] });
   }
 
@@ -17,20 +17,29 @@ class UserController {
     const { firstName, lastName, email, password } = req.body;
     const errors = [];
 
-    if(!v.user.firstName(firstName)) {
+    const alreadyExist = await User.findOne({
+      where: {
+        email: email,
+      }
+    })
+    
+    if (alreadyExist) {
+      errors.push('Cet email est déja utilisé');
+    }
+    if (!v.user.firstName(firstName)) {
       errors.push('Merci de renseigner un prénom valide.');
     }
-    if(!v.user.lastName(lastName)) {
+    if (!v.user.lastName(lastName)) {
       errors.push('Merci de renseigner un nom valide.');
     }
-    if(!v.user.email(email)) {
+    if (!v.user.email(email)) {
       errors.push('Merci de renseigner un email valide.');
     }
-    if(!v.user.password(password)) {
+    if (!v.user.password(password)) {
       errors.push('Merci de renseigner un mot de passe valide. (8 caractères, 1 lettre, 1 nombre et 1 caractères spécial.');
     }
 
-    if(errors.length === 0) {
+    if (errors.length === 0) {
       const user = await User.create({
         roleId: 2, // 1 pour admin, 2 pour user
         firstName,
@@ -69,28 +78,28 @@ class UserController {
     const { userId } = req.params;
     const errors = [];
 
-    if(firstName && !v.user.firstName(firstName)) {
+    if (firstName && !v.user.firstName(firstName)) {
       errors.push('Merci de renseigner un prénom valide.');
     }
-    if(lastName && !v.user.lastName(lastName)) {
+    if (lastName && !v.user.lastName(lastName)) {
       errors.push('Merci de renseigner un nom valide.');
     }
-    if(email && !v.user.email(email)) {
+    if (email && !v.user.email(email)) {
       errors.push('Merci de renseigner un email valide.');
     }
-    if(password && !v.user.password(password)) {
+    if (password && !v.user.password(password)) {
       errors.push('Merci de renseigner un mot de passe valide. (8 caractères, 1 lettre, 1 nombre et 1 caractères spécial.');
     }
 
-    if(errors.length === 0) {
+    if (errors.length === 0) {
       const payload = {};
 
-      if(firstName) payload.firstName = firstName;
-      if(lastName) payload.lastName = lastName;
-      if(email) payload.email = email;
-      if(password) payload.password = password;
+      if (firstName) payload.firstName = firstName;
+      if (lastName) payload.lastName = lastName;
+      if (email) payload.email = email;
+      if (password) payload.password = password;
 
-      const user = await User.update(payload, { 
+      const user = await User.update(payload, {
         where: { id: userId },
         returning: true,
         plain: true,
@@ -104,10 +113,10 @@ class UserController {
 
   static async login(req, res, next) {
     const { email, password } = req.body;
-    
-    const user = await User.findOne({ where: { email }});
 
-    if(user && password && user.correctPassword(password)) {
+    const user = await User.findOne({ where: { email } });
+
+    if (user && password && user.correctPassword(password)) {
       const payload = {
         id: user.id,
         roleId: user.roleId,
@@ -117,9 +126,9 @@ class UserController {
       };
       const token = createJWT(payload);
 
-      res.json({code: 200, msg: 'Bienvenue !', data: { user, token, }, errors: []});
+      res.json({ code: 200, msg: 'Bienvenue !', data: { user, token, }, errors: [] });
     } else {
-      res.json({code: 403, msg: 'Identifiants incorrects.', data: {}, errors: []});
+      res.json({ code: 403, msg: 'Identifiants incorrects.', data: {}, errors: [] });
     }
   }
 }
